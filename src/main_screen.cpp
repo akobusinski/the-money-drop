4482985f643c8c8c5aca86d72920b93d3453b18d
Antoni Kobusiński <60139174+akobusinski@users.noreply.github.com>
2026-05-24 19:31:25 +0200

    Implement a basic main menu screen

--- src/main_screen.cpp
+++ src/main_screen.cpp
@@ -1,7 +1,26 @@
+#include <iostream>
 #include <print>
 #include "screen.hpp"
 
 ScreenAction MainScreen::Show() {
-    std::println("hi");
-    return PushScreen { std::make_unique<GameScreen>() };
+    std::println("Gra Postaw Na Milion");
+    std::println("Prosze wybrac jedna z opcji:");
+    std::println("1. Nowa gra");
+    std::println("2. Wyjscie");
+    std::print("> ");
+    int chosen;
+    std::cin >> chosen;
+    if (!std::cin) {
+        std::println("Nieprawidlowa opcja");
+        return CloseScreen {};
+    }
+
+    if (chosen == 1) {
+        return PushScreen { std::make_unique<GameScreen>() };
+    } else if (chosen == 2) {
+        return CloseScreen {};
+    }
+
+    std::println("Nieprawdlowa opcja");
+    return CloseScreen {};
 }
