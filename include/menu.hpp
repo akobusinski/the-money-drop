35a387d34eed0d19351e6a1eeef76c2b2f36cdc0
Antoni Kobusiński <60139174+akobusinski@users.noreply.github.com>
2026-05-29 17:13:44 +0200

    don't inline ReadSizeInRange

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <iostream>
-#include <limits>
 #include <print>
 #include <span>
 #include <string_view>
@@ -11,29 +10,7 @@ struct MenuItem {
     T value;
 };
 
-inline std::size_t ReadSizeInRange(std::string_view input_label, std::size_t min, std::size_t max) {
-    if (!input_label.empty()) {
-        std::println("{}", input_label);
-    }
-
-    while (true) {
-        std::print("> ");
-
-        std::size_t choice;
-        std::cin >> choice;
-
-        if (std::cin) {
-            if (choice >= min && choice <= max) {
-                return choice;
-            }
-        } else {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-
-        std::println("Niepoprawny wybor, sprobuj jeszcze raz.");
-    }
-}
+std::size_t ReadSizeInRange(std::string_view input_label, std::size_t min, std::size_t max);
 
 template <typename T>
 T ChooseFromMenu(std::string_view menu_label, const std::span<MenuItem<T>> &items) {
